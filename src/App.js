bb70068ec1836ed6f5b8a804d48f0ff8ebdf87ce
jehyungmin <wpaudrl@gmail.com>
2019-04-04 06:51:06 +0900

    번호 띄우기까지

--- src/App.js
+++ src/App.js
@@ -4,52 +4,70 @@ import axios from 'axios';
 import Lotto from './Components/Lotto';
 
 class App extends Component {
-  state ={
+  static defaultProps ={
+    lotto: []
+  }
 
+  constructor(props) {
+    super(props);
+    this.state ={
+      lotto:[]
+    };
+  }
+
+  componentWillMount() {
+    console.log("componentWillMount");
+    axios.get('/common.do?method=getLottoNumber&drwNo=852').then(res => {
+      this.setState({lotto:res.data});
+    });
   }
 
   componentDidMount() {
     // axios.get('/common.do?method=getLottoNumber&drwNo=852')
     // .then(res => console.log(res))
     // .catch(err => console.log(err))
-    this._getLotto();
+    //this._getLotto();
+    console.log("componentDidMount");
   }
 
-  _getLotto = async () => {
-    const lotto = await this._collApi();
-    this.setState({
-      lotto
-    });
-  }
+  // _getLotto = async () => {
+  //   const lotto = await this._collApi();
+  //   this.setState({
+  //     lotto
+  //   });
+  // }
 
-  _collApi = () => {
-    return axios.get('/common.do?method=getLottoNumber&drwNo=852')
-      .then(response => console.log('response : ', response))
-      //.then(response => response.json())
-      //.then(json => json.data)
-      .catch(err => console.log(err))
-  }
+  // _collApi = () => {
+  //   return axios.get('/common.do?method=getLottoNumber&drwNo=852')
+  //     .then(response => console.log('response : ', response))
+  //     .then(response => response.json())
+  //     .then(json => json.data)
+  //     //.then(json => json.data)
+  //     .catch(err => console.log(err))
+  // }
 
   _renderLotto = () => {
-    const lotto = this.state.lotto.map((data) => {
+    //const lotto = this.state.lotto.map((data) => {
       return <Lotto 
-        num1={data.drwtNo1}
-        num2={data.drwtNo2}
-        num3={data.drwtNo3}
-        num4={data.drwtNo4}
-        num5={data.drwtNo5}
-        num6={data.drwtNo6}
-        bnus={data.bnusNo}
+        num1={this.state.lotto.drwtNo1}
+        num2={this.state.lotto.drwtNo2}
+        num3={this.state.lotto.drwtNo3}
+        num4={this.state.lotto.drwtNo4}
+        num5={this.state.lotto.drwtNo5}
+        num6={this.state.lotto.drwtNo6}
+        bnus={this.state.lotto.bnusNo}
       />
-    })
-    return lotto;
+    //})
+    //return lotto;
   }
 
   render() {
     const {lotto} = this.state;
+    console.log("123", this.state.lotto)
     return (
       <div className={lotto ? "App" : "App-logging"}>
         {this.state.lotto ? this._renderLotto() : "logding"} 
+        {/* {this._renderLotto()} */}
       </div>
     );
   }
--- src/Components/Lotto.js
+++ src/Components/Lotto.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Lotto = () => {
+const Lotto = ({num1,num2,num3,num4,num5,num6,bnus}) => {
     return (
         <div>
-            
+            {num1}
+            {num2}
+            {num3}
+            {num4}
+            {num5}
+            {num6}
+            {bnus}
         </div>
     );
 };
 
-export default Lotto;
+export default Lotto; 
