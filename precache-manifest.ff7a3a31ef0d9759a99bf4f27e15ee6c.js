226b632b7631e28f57cd6222cffcc14fb798c992
jehyungmin <wpaudrl@gmail.com>
2019-04-18 17:42:19 +0900

    Updates

---
+++ precache-manifest.ff7a3a31ef0d9759a99bf4f27e15ee6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb4a81a525af85fc5935",
+    "url": "/react-lotto-application/static/css/main.9aee2e59.chunk.css"
+  },
+  {
+    "revision": "bb4a81a525af85fc5935",
+    "url": "/react-lotto-application/static/js/main.57e1618c.chunk.js"
+  },
+  {
+    "revision": "2dae33ec7db7f168faf1",
+    "url": "/react-lotto-application/static/js/runtime~main.7c20bdc7.js"
+  },
+  {
+    "revision": "0cbe79d670472c435a2e",
+    "url": "/react-lotto-application/static/js/2.40cf2513.chunk.js"
+  },
+  {
+    "revision": "03bac060f004a435edbb4bec45d8cc00",
+    "url": "/react-lotto-application/index.html"
+  }
+];
